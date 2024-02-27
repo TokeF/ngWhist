c336439b6048698b0af3dec405ebcde816e3df01
TokeF <42637648+TokeF@users.noreply.github.com>
2024-02-27 16:12:12 +0100

    add selector for bet and bet type

--- src/app/whist-game/whist-game.component.ts
+++ src/app/whist-game/whist-game.component.ts
@@ -1,29 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { WhistDataService } from '../whist-data.service';
-import { FormArray, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import {CdkTableModule, DataSource} from '@angular/cdk/table';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Player } from '../player';
+import { NgFor } from '@angular/common';
+
+export enum Bets {
+  Vip = "Vip",
+  Halve = "Halve",
+  Sans = "Sans",
+  Gode = "Gode",
+  Sol = "Sol",
+  RenSol = "Ren Sol",
+  Bordlægger = "Bordlægger",
+}
 
 @Component({
   selector: 'app-whist-game',
   standalone: true,
-  imports: [CdkTableModule],
+  imports: [
+    CdkTableModule,
+    ReactiveFormsModule,
+    NgFor,
+  ],
   templateUrl: './whist-game.component.html',
   styleUrl: './whist-game.component.css'
 })
+
 export class WhistGameComponent implements OnInit {
   
   private names : string[] = [];
   private scoreSystem : string = '';
+  form: FormGroup = new FormGroup({});
   dataSource : BehaviorSubject<any> = new BehaviorSubject<any>([]);
   players = new BehaviorSubject<{[name: string]: Player}>({});
   displayedColumns: string[] = ['name', 'score'];
+  Bets :Bets[] = [Bets.Vip, Bets.Halve, Bets.Sans, Bets.Gode, Bets.Sol, Bets.RenSol, Bets.Bordlægger];
 
   constructor(
       private router: Router,
-      private whistDataService: WhistDataService
+      private whistDataService: WhistDataService,
+      private formBuilder: FormBuilder
     ) { 
       this.whistDataService.currentGameData.subscribe(form => {
       this.names = form.get('names')?.value;
@@ -32,7 +51,7 @@ export class WhistGameComponent implements OnInit {
   }
 
   ngOnInit() {
-    
+    // Table
     this.names.forEach(name => {
       if(name != ""){
         this.players.value[name] = {name: name, score: 0};
@@ -40,6 +59,11 @@ export class WhistGameComponent implements OnInit {
     });
     this.players.subscribe(player => this.dataSource.next(Object.values(player)));
     
+    //Form
+    this.form = new FormGroup({
+      betAmount: this.formBuilder.control(7),
+      betType: this.formBuilder.control('Vip'),
+    });
   }
 
   addPlay() {
